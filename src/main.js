9bb8063f27f5412364c84acbffba81f92dd2a334
meetmiss <50589594+meetmiss@users.noreply.github.com>
2019-08-01 20:49:37 +0800

    项目构建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import router from '@/router'
+// 不属于js/vue/json，所以需要写完整路径
+import '@/style/index.less'
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        login
+        <el-card class="mycard">
+            <img src="@/assets/images/logo_index.png" alt="">
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+.container{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: url('../../assets/images/login_bg.jpg') no-repeat center / cover;
+}
+.mycard{
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -60%);
+    img {
+        display: block;
+        width: 200px;
+        margin: 0 auto;
+    }
+}
+</style>
