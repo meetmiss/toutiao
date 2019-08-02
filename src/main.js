b79e8524044e2f28a196c5ed620386573f484ba1
meetmiss <50589594+meetmiss@users.noreply.github.com>
2019-08-02 18:10:48 +0800

    login页面表单

--- src/main.js
+++ src/main.js
@@ -7,9 +7,11 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
 // 不属于js/vue/json，所以需要写完整路径
 import '@/style/index.less'
+import axios from 'axios'
 
 Vue.use(ElementUI)
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   render: h => h(App),
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,36 +1,98 @@
 <template>
-    <div class="container">
-        login
-        <el-card class="mycard">
-            <img src="@/assets/images/logo_index.png" alt="">
-        </el-card>
-    </div>
+  <div class="container">
+    <el-card class="mycard">
+      <img src="@/assets/images/logo_index.png" alt />
+      <el-form ref="loginForm" :model="loginForm" :rules="loginRules" status-icon>
+        <el-form-item prop="mobile">
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item prop="code">
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:236px;margin-right:12px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click="login">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    // 校验函数
+    var checkMobile = (rule, value, callback) => {
+      if (!/^1[3-9]\d{9}$/.test(value)) {
+        return callback(new Error('手机号格式不对'))
+      }
+      callback()
+    }
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      },
+      // 校验
+      loginRules: {
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '请输入验证码', trigger: 'blur' },
+          { len: 6, message: '验证码长度为6位', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    login () {
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          this.$http
+            .post(
+              'http://ttapi.research.itcast.cn/mp/v1_0/authorizations',
+              this.loginForm
+            )
+            .then(res => {
+              this.$router.push('/')
+            })
+            .catch(() => {
+              this.$message.error('手机号或验证码有误')
+            })
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
-.container{
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background: url('../../assets/images/login_bg.jpg') no-repeat center / cover;
+.container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url("../../assets/images/login_bg.jpg") no-repeat center / cover;
 }
-.mycard{
-    width: 400px;
-    height: 350px;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -60%);
-    img {
-        display: block;
-        width: 200px;
-        margin: 0 auto;
-    }
+.mycard {
+  width: 400px;
+  height: 350px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -60%);
+  img {
+    display: block;
+    width: 200px;
+    margin: 0 auto 30px;
+  }
 }
 </style>
