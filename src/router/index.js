b14474746da5016cbbd6e2d16ee6e07ab607c2b5
meetmiss <50589594+meetmiss@users.noreply.github.com>
2019-08-02 22:05:43 +0800

    Home page to complete

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,9 @@ import VueRouter from 'vue-router'
 import Vue from 'vue'
 
 import Login from '@/views/login'
+import Home from '@/views/home'
+import Welcome from '@/views/welcome'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
@@ -10,6 +13,20 @@ const router = new VueRouter({
       path: '/login',
       name: 'login',
       component: Login
+    },
+    {
+      path: '/',
+      // home和welcome的name会冲突，需注释
+      // name: 'home',
+      component: Home,
+      children: [
+        {
+          path: '',
+          name: 'welcome',
+          component: Welcome
+
+        }
+      ]
     }
   ]
 })
---
+++ src/views/home/index.vue
@@ -0,0 +1,137 @@
+<template>
+  <el-container class="wrapper">
+    <el-aside :width="isCollapse?'64px':'200px'">
+      <div class="log" :class="{miniLogo:isCollapse}"></div>
+
+      <el-menu
+        default-active="1"
+        class="el-menu-vertical-demo"
+        background-color="#002033"
+        text-color="#fff"
+        active-text-color="#ffd04b"
+        :collapse="isCollapse"
+        :collapse-transition="false"
+      >
+        <el-menu-item index="1">
+          <i class="el-icon-s-home"></i>
+          <span slot="title">首页</span>
+        </el-menu-item>
+        <el-menu-item index="2">
+          <i class="el-icon-document"></i>
+          <span slot="title">内容管理</span>
+        </el-menu-item>
+        <el-menu-item index="3">
+          <i class="el-icon-picture"></i>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+        <el-menu-item index="4">
+          <i class="el-icon-s-promotion"></i>
+          <span slot="title">发布文章</span>
+        </el-menu-item>
+        <el-menu-item index="5">
+          <i class="el-icon-chat-dot-round"></i>
+          <span slot="title">评论管理</span>
+        </el-menu-item>
+        <el-menu-item index="6">
+          <i class="el-icon-present"></i>
+          <span slot="title">粉丝管理</span>
+        </el-menu-item>
+        <el-menu-item index="7">
+          <i class="el-icon-setting"></i>
+          <span slot="title">个人设置</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <span class="el-icon-s-fold" @click="toggleMenu"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            <img src="../../assets/images/avatar.jpg" alt />
+            用户名称
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <span class="el-icon-setting"></span>
+              个人设置
+            </el-dropdown-item>
+            <el-dropdown-item>
+              <span class="el-icon-unlock"></span>
+              退出登录
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.isCollapse = !this.isCollapse
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  .el-aside {
+    // height: 100%;
+    background-color: #002033;
+  }
+  .el-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .el-icon-s-fold {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      padding-left: 10px;
+      vertical-align: middle;
+    }
+    .el-dropdown {
+      float: right;
+      .el-dropdown-link {
+        font-weight: 700;
+        img {
+          width: 30px;
+          height: 30px;
+          vertical-align: middle;
+        }
+      }
+    }
+  }
+  .log {
+    width: 100%;
+    height: 60px;
+    background: #002244 url(../../assets/images/logo_admin.png) no-repeat center /
+      140px auto;
+  }
+  .miniLogo {
+    background-image: url(../../assets/images/logo_admin_01.png);
+    background-size: 36px auto;
+  }
+  .el-menu {
+    border-right: none;
+  }
+}
+</style>
